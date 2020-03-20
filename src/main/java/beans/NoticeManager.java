88950a58089d69c65d820c6d24f7c4697c5cc8ae
Chelsea <chelsea.l.amman@gmail.com>
2020-03-20 19:09:49 -0400

    -Add JMS resource calls to NoticeManager -Add mesg() function to NoticeManager

--- src/main/java/beans/NoticeManager.java
+++ src/main/java/beans/NoticeManager.java
@@ -2,17 +2,31 @@ package beans;
 
 import java.io.Serializable;
 
+import javax.annotation.Resource;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import javax.jms.*;
+
 
 @SessionScoped
 @Named("noticeMgr")
 public class NoticeManager implements Serializable {
 
-	
+
+
 	private static final long serialVersionUID = 1L;
 
 	private NoticeOfArrival notice = new NoticeOfArrival();
+	@Resource(mappedName = "jms/myLog")
+	private Queue logMessages;
+	@Resource(mappedName = "jms/TopicOne")
+	private Topic TopicOne;
+	@Resource(mappedName = "jms/TopicTwo")
+	private Topic TopicTwo;
+	@Resource(mappedName = "jms/myMessageFactory")
+	private ConnectionFactory logFactory;
+
+
 	
 	public void setNotice( NoticeOfArrival notice ) {
 		this.notice = notice;
@@ -22,6 +36,15 @@ public class NoticeManager implements Serializable {
 	public NoticeOfArrival getNotice() { return notice ; }
 
 	
-	public void mesg() { System.out.println("NoticeManager: mesg()");}
-
+	public void mesg() {
+		System.out.println("NoticeManager: mesg()");
+		try (
+				JMSContext context = logFactory.createContext()) {
+				JMSProducer mp = context.createProducer();
+				Message tm = context.createTextMessage();
+				mp.send(logMessages, tm);
+		} catch (Exception e) {
+			System.out.println("Error:" + e);
+		}
+	}
 }

cafea2e263c927ba8afd5fa9bc020828a03c2784
Chelsea <chelsea.l.amman@gmail.com>
2020-03-20 23:46:03 -0400

    -Sort of finished PortAuthority

--- src/main/java/beans/EntityManager.java
+++
@@ -1,7 +0,0 @@
-package beans;
-
-public class EntityManager {
-
-    public void persist(NoticeOfArrival noa){};
-    //TODO: This is a stub used by PortAuthority
-}
--- src/main/java/beans/PortAuthority.java
+++ src/main/java/beans/PortAuthority.java
@@ -21,27 +21,19 @@ public class PortAuthority implements MessageListener {
 
     NoticeManager notice = new NoticeManager();
 
-    private void sendMessage(String msg){
-        //TODO: whoever implements the factory will also need to implement sendMessage. Please send a message to topic1 and topic in their respective mappings
-        //look at BookManagementFacadeBean.java in Renet's examples for reference
-    }
-
     @Override
     public void onMessage(Message msg) {
         if (msg instanceof TextMessage) {
             TextMessage tm = (TextMessage) msg;
             try {
                 String text = tm.getText();
-                EntityManager em = new EntityManager();
                 NoticeOfArrival noa = deserialize(text);
                 if (noa.getClient() == 1 | noa.getClient() == 3){
-                    //sendMessage(topic1, blah blah blah)
+                    notice.sendMesg(text);
                 }
                 else if (noa.getClient() == 2){
-                    //sendMessage(topic2, blah blah blah)
+                    notice.sendMesg(text);
                 }
-                em.persist(noa);
-
             } catch (Exception e) {
                 System.out.println("Message bean error: " + e);
             }
